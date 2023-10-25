30146458bd0e27f4e8c77f0adff58ea6cd1865b5
Andrey Carvalho <andreycarvalho@icloud.com>
2023-10-25 06:21:39 -0300

    First Challenge Solution

---
+++ Module 02 - Databases & Git/init.sql
@@ -0,0 +1,18 @@
+DROP TABLE books;
+DROP TABLE authors;
+
+
+CREATE TABLE IF NOT EXISTS authors (
+	id UUID PRIMARY KEY DEFAULT gen_random_uuid(), -- gen_random_uuid only available on Postgres 13+
+  name VARCHAR(100) NOT NULL ,
+ 	nationality VARCHAR(60) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS books (
+	id UUID PRIMARY KEY DEFAULT gen_random_uuid()	,
+  title  VARCHAR(255) NOT NULL ,
+ 	publication_year INT NOT NULL ,
+  author_id UUID  NOT NULL REFERENCES authors(id) ON DELETE CASCADE,
+  UNIQUE(title)
+);
---
+++ Module 02 - Databases & Git/queries.sql
@@ -0,0 +1,80 @@
+-- write a query to list all books published in 2020
+
+SELECT * 
+FROM books
+WHERE publication_year = 2020;
+
+
+-- write a query to list all authors who have at least 2 books on the library
+
+SELECT a.name name
+FROM authors a 
+LEFT JOIN books b ON b.author_id = a.id
+GROUP BY a.id
+HAVING COUNT(b.title) > 1;
+
+
+-- write a query to find the author with most books on the library
+
+SELECT a.name name
+FROM authors a 
+LEFT JOIN books b ON b.author_id = a.id
+GROUP BY a.id
+ORDER BY COUNT(b.title) DESC
+LIMIT 1;
+
+
+-- write a query to find the name of the author and the most recent books
+
+WITH temporary_table AS (
+  SELECT b.author_id author_id, MAX(b.publication_year) publication_year
+	FROM books b
+	GROUP BY b.author_id
+)
+SELECT a.name name, b.title
+FROM authors a 
+LEFT JOIN books b ON b.author_id = a.id
+LEFT JOIN temporary_table tt ON tt.author_id = a.id
+WHERE tt.author_id = b.author_id AND tt.publication_year = b.publication_year;
+
+
+-- write a query to find how many books each author have write
+
+SELECT a.name name, COUNT(b.title) book_count 
+FROM authors a 
+LEFT JOIN books b ON b.author_id = a.id
+GROUP BY a.id
+ORDER BY book_count DESC;
+
+
+-- write a query to list all books and their respective authors alfabetically ordinated
+
+SELECT b.title title, a.name
+FROM authors a 
+LEFT JOIN books b ON b.author_id = a.id
+ORDER BY a.name ASC;
+
+-- write a query to calculate the average of all books publication year
+
+SELECT FLOOR(AVG(b.publication_year)) average
+FROM books b;
+
+-- write a query to find the title of the olddest book on the library 
+
+SELECT b.title
+FROM books b
+ORDER BY b.publication_year ASC
+LIMIT 1;
+
+-- write a query to find the total count of books on the library
+
+SELECT COUNT(*) total_books_count
+from books;
+
+-- write a query to find all authors who havent books at the library 
+
+SELECT a.name name
+FROM authors a 
+LEFT JOIN books b ON b.author_id = a.id
+GROUP BY a.id
+HAVING COUNT(b.title) < 1;
