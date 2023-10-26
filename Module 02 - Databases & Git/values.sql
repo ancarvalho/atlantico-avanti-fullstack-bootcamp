5a7d66016826b22395958755bdfc9f2d356e30cb
Andrey Carvalho <andreycarvalho@icloud.com>
2023-10-26 20:36:26 -0300

    error fix

--- Module 02 - Databases & Git/values.sql
+++ Module 02 - Databases & Git/values.sql
@@ -10,10 +10,10 @@ insert into "authors" ("id", "name", "nationality") values ('c1aef9f9-6aad-4b67-
 
 
 
-insert into "books" ("author_id", "publication_year", "title") values ('5ef474a-ffba-4886-8d4a-155b443f199f', 1944, 'The Road to Serfdom');
+insert into "books" ("author_id", "publication_year", "title") values ('95ef474a-ffba-4886-8d4a-155b443f199f', 1944, 'The Road to Serfdom');
 
 insert into "books" ("author_id", "publication_year", "title") values ('43e9c754-39ac-4cc3-bd1d-b53bd90153e8', 1973, 'The Machinery of Freedom');
-insert into "books" ("author_id", "publication_year", "title") values ('43e9c754-39ac-4cc3-bd1d-b53bd90153e8', 2000, "Law's Order");
+insert into "books" ("author_id", "publication_year", "title") values ('43e9c754-39ac-4cc3-bd1d-b53bd90153e8', 2000, 'Laws Order');
 
 insert into "books" ("author_id", "publication_year", "title") values ('06c6cbf9-4e68-4e82-af57-b077dd5ab5a0', 1962, 'Capitalism and Freedom');
 insert into "books" ("author_id", "publication_year", "title") values ('06c6cbf9-4e68-4e82-af57-b077dd5ab5a0', 1980, 'Free to Choose');
@@ -21,3 +21,6 @@ insert into "books" ("author_id", "publication_year", "title") values ('06c6cbf9
 
 insert into "books" ("author_id", "publication_year", "title") values ('82d0608c-912f-4d93-a06d-14436033ecf2', 1927, 'Liberalism');
 insert into "books" ("author_id", "publication_year", "title") values ('82d0608c-912f-4d93-a06d-14436033ecf2', 1956, 'The Anti-Capitalistic Mentality');
+
+
+
